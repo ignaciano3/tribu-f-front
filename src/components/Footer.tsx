81e9078dda9da184b7006dc8d67dcb24766d169e
ignacio <igarciap@fi.uba.ar>
2023-11-20 11:21:40 -0300

    cambio un poco el footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,7 +1,7 @@
 const Footer = () => {
   return (
-    <footer className="py-10 flex justify-center items-center ">
-      Hecho por Tribu F 
+    <footer className="py-10 flex justify-center items-center bg-gray-100 ">
+      Hecho con ❤️ por Tribu F 
     </footer>
   );
 };
