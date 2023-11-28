969c32e5ed62fd6bde33894c606b63601ad8cf31
ignacio <igarciap@fi.uba.ar>
2023-11-27 22:14:44 -0300

    cambios

--- next.config.js
+++ next.config.js
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  env: {
+    soporteApiUrl: "https://backendsupport-dev-jfnc.4.us-1.fl0.io/",
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
--- src/app/tickets/[id]/page.tsx
+++ src/app/tickets/[id]/page.tsx
@@ -1,21 +1,16 @@
-import { useRouter } from 'next/router';
-import { useEffect, useState } from 'react';
-
 async function getTicket(id: string) {
   const response = await fetch(`/api/tickets/${id}`);
   const data = await response.json();
   return data;
 }
 
-export default function TicketPage () {
-  const router = useRouter();
-  const id = router.query.id as string;
-
+export default function TicketPage ({ params }: { params: { id: string } }) {
+  const ticket = { id: params.id, title: 'Ticket Title', description: 'Ticket Description' };
   return (
     <div>
       <h1>Ticket Information</h1>
-      <p>ID: {ticket.id}</p>
-      <p>Title: {ticket.title}</p>
+      <p>ID: {params.id}</p>
+      <p>Title: {ticket?.title}</p>
       <p>Description: {ticket.description}</p>
     </div>
   )
