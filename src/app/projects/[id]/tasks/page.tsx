1d3412cdbb07ba0be4b4325313e154c8391b3c05
ricomateo <mrico@fi.uba.ar>
2023-11-30 12:02:57 -0300

    agregamos actualizacion de la lista de tareas

--- src/app/projects/[id]/tasks/page.tsx
+++ src/app/projects/[id]/tasks/page.tsx
@@ -1,9 +1,11 @@
+"use client";
 import { Task } from "@/types/types";
 import TaskGridRow from "@/components/taskGridRow";
 import Button from "@/components/button";
 import Table from "@/components/Table/Table";
 import Title from "@/components/Title";
 import Nuevo from "@/components/Nuevo";
+import React, { useState, useEffect } from "react";
 
 const getTasks = async (id: string) => {
   console.log("id: ", id);
@@ -31,18 +33,34 @@ const headers = [
 ];
 
 export default async function TaskGrid({ params }: { params: { id: string } }) {
-  const list = await getTasks(params.id);
+  const [tasks, setTasks] = useState([]);
+  useEffect(() => {
+    // Función para cargar tareas
+    const loadTasks = async () => {
+      try {
+        const loadedTasks = await getTasks(params.id);
+        setTasks(loadedTasks);
+      } catch (error) {
+        console.error("Error al cargar proyectos:", error);
+      }
+    };
+
+    // Llamar a la función al cargar la página
+    loadTasks();
+  }, []);
+  /*const list = await getTasks(params.id);
   console.log("list: ", list);
   const id = params.id;
   console.log("id: ", id);
-  const href = "/projects/" + id + "/tasks/create";
+  */
+  const href = "/projects/" + params.id + "/tasks/create";
   return (
     <>
       <div className="flex justify-between">
         <Title title="Tareas" className="inline" />
         <Nuevo title="Crear nueva tarea" href={href} />
       </div>
-      <Table data={list} headers={headers} RowItem={TaskGridRow} />
+      <Table data={tasks} headers={headers} RowItem={TaskGridRow} />
     </>
   );
 }
