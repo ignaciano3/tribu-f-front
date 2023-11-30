100cc73baed3348ff312ff2ed5ad7c7e9a27ec9a
ignacio <igarciap@fi.uba.ar>
2023-11-30 20:28:30 -0300

    avanzo con todo

--- src/api/soporte.ts
+++ src/api/soporte.ts
@@ -16,9 +16,20 @@ export async function getTickets(idProd: string) {
 }
 
 export async function getProducts() {
+  const response = await fetch(process.env.soporteApiUrl + `product`, {
+    next: { revalidate: DAY },
+  });
+  if (response.ok) {
+    const data = await response.json();
+    return data;
+  } else {
+    throw new Error("Hubo un error en el back");
+  }
+}
+
+export async function getProduct(idProd: Number) {
   const response = await fetch(
-    process.env.soporteApiUrl + `product`,
-    { next: { revalidate: DAY } }
+    process.env.soporteApiUrl + `product/${idProd}`,
   );
   if (response.ok) {
     const data = await response.json();
@@ -28,21 +39,31 @@ export async function getProducts() {
   }
 }
 
-export async function createTicket(formData : FormData) {
-  'use server'
-  console.log(formData)
-  const response  = await fetch(
-    process.env.soporteApiUrl + `ticket`,
-    {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify(ticket),
-    }
+export async function getTicket(idProd : Number) {
+  const response = await fetch(
+    process.env.soporteApiUrl + `ticket/${idProd}`,
   );
+  if (response.ok) {
+    const data = await response.json();
+    return data;
+  } else {
+    throw new Error("Hubo un error en el back");
+  }
+}
 
-  if (response.ok) { // Revisar si recibe algo
+export async function createTicket(formData: FormData) {
+  "use server";
+  console.log(formData);
+  const response = await fetch(process.env.soporteApiUrl + `ticket`, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(ticket),
+  });
+
+  if (response.ok) {
+    // Revisar si recibe algo
     const data = await response.json();
     return data;
   } else {
--- src/components/Table/Table.tsx
+++ src/components/Table/Table.tsx
@@ -33,11 +33,8 @@ export default function Table(props: TableProps) {
   const RowItem = props.RowItem ?? DefaultRowItem;
   const HeaderItem = props.HeaderItem ?? DefaultHeaderItem;
   const headers = props.headers ?? Object.keys(data[0]);
-  console.log("typeof data: ", typeof data);
   return (
-    <div className="container max-w-7xl mx-auto mt-8">
-      <div className="flex flex-col">
-        <div className="overflow-x-auto sm:-mx-6 sm:px-6 lg:-mx-8 lg:px-8">
+    <div className="container max-w-7xl mx-auto mt-8 flex flex-col overflow-x-auto sm:-mx-6 sm:px-6 lg:-mx-8 lg:px-8">
           <div className="inline-block min-w-full overflow-hidden align-middle border-b border-gray-200 shadow sm:rounded-lg">
             <table className="min-w-full">
               <thead>
@@ -55,8 +52,7 @@ export default function Table(props: TableProps) {
               </tbody>
             </table>
           </div>
-        </div>
       </div>
-    </div>
-  );
+      );
+  
 }
