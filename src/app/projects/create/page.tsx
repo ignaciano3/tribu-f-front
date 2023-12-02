36b67b9a0a85a06966c2df9fd838476d46e85f9e
ricomateo <mrico@fi.uba.ar>
2023-12-01 21:21:36 -0300

    cambio url de getUsuarios

--- src/app/projects/create/page.tsx
+++ src/app/projects/create/page.tsx
@@ -5,33 +5,19 @@ import { Usuario } from "@/types/types";
 const getUsuarios = async () => {
   // esto se lo tendria que mejorar metiendo variables
   // de entorno y sacandolo a un helper
-
-  const response = await fetch(
-    "https://anypoint.mulesoft.com/mocking/api/v1/sources/exchange/assets/754f50e8-20d8-4223-bbdc-56d50131d0ae/recursos-psa/1.0.0/m/api/recursos"
-  );
+  const url = process.env.proyectosApiUrl + "employees/get_employees";
+  const response = await fetch(url);
   const data = await response.json();
   return data;
 };
 
-export default function CreateProjectPage() {
-  //const usuarios = await getUsuarios();
-  //const usuarios =
-  /*const usuarios: { nombre: string; apellido: string; legajo: number }[] = [
-    { nombre: "Juan", apellido: "Pérez", legajo: 123 },
-    { nombre: "María", apellido: "Gómez", legajo: 456 },
-    { nombre: "Carlos", apellido: "Rodríguez", legajo: 789 },
-  ];
-  */
-  const usuarios: Usuario[] = [
-    { legajo: 1, nombre: "Ignacio", apellido: "García" },
-    { legajo: 2, nombre: "Nico", apellido: "Ronchese" },
-    { legajo: 3, nombre: "Saul", apellido: "Goodman" },
-    // Puedes agregar más líderes según sea necesario
-  ];
-  console.log("typeof usuarios en CreateProjectPage: ", typeof usuarios);
+const CreateProjectPage = async () => {
+  const employees: any[] = await getUsuarios();
   return (
     <>
-      <ProjectForm usuarios={usuarios}></ProjectForm>
+      <ProjectForm employees={employees}></ProjectForm>
     </>
   );
-}
+};
+
+export default CreateProjectPage;
