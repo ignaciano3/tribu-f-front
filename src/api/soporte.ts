f10d4a86a4559275470711ee0619e341597a6e35
ignacio <igarciap@fi.uba.ar>
2023-12-04 18:38:04 -0300

    cambio users

--- src/api/soporte.ts
+++ src/api/soporte.ts
@@ -13,13 +13,10 @@ export async function GetClients() {
 }
 
 export async function GetUsers() {
-  const url = "user/";
-  return await useFetch({
-    url: url,
-    soporte: true,
-    revalidate: true,
-    tags: ["users"],
-  });
+  const url = "https://anypoint.mulesoft.com/mocking/api/v1/sources/exchange/assets/754f50e8-20d8-4223-bbdc-56d50131d0ae/recursos-psa/1.0.0/m/api/recursos";
+  const data = await fetch(url);
+  const users = await data.json();
+  return users;
 }
 
 export async function GetTickets(versionId: string) {
